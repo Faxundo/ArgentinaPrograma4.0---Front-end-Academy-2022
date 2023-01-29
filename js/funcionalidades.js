6d86307224860d04b7bd8f6e96cbeba5510d5293
Facundo <faxundoc@gmail.com>
2023-01-29 12:39:31 -0300

    Funcionalidad de iconos inferiores(barra lateral, links y descarga). Icono de página

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,5 +1,20 @@
+var barra = document.getElementById("desplegable");
+var descargar_pagina = document.getElementById("descargar_pagina");
+var perfil_profesional = document.getElementById("perfil_profesional");
+var estado = barra.dataset.estado;
+
+perfil_profesional.addEventListener('click', function(){
+    if (estado == 0) {
+        barra.style.display = "block";
+        estado = 1;
+    } else {
+        barra.style.display = "none";
+        estado = 0;
+    }
+});
 
 
-document.getElementById("linkedin_link").addEventListener('click', function{
+descargar_pagina.addEventListener('click', function(){
+    window.print();
+});
 
-})
